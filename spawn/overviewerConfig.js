832d30e5c4dcff03199f2da2d908c049c4caa138
itskatt <raph10@mail.com>
2022-07-06 18:29:24 +0200

    add spawn

---
+++ spawn/overviewerConfig.js
@@ -0,0 +1,136 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "La Falaise - overworld"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1652564977",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Jour_Nord",
+            "zoomLevels": 7,
+            "defaultZoom": 3,
+            "maxZoom": 7,
+            "path": "jour-nord",
+            "base": "",
+            "bgcolor": "#333333",
+            "world": "La Falaise - overworld",
+            "last_rendertime": 1652533413,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                188,
+                64,
+                426
+            ],
+            "minZoom": 3,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "Jour_Ouest",
+            "zoomLevels": 6,
+            "defaultZoom": 3,
+            "maxZoom": 6,
+            "path": "jour-ouest",
+            "base": "",
+            "bgcolor": "#333333",
+            "world": "La Falaise - overworld",
+            "last_rendertime": 1652533413,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                188,
+                64,
+                426
+            ],
+            "minZoom": 3,
+            "spawn": false,
+            "north_direction": 1
+        },
+        {
+            "name": "Jour_Sud",
+            "zoomLevels": 7,
+            "defaultZoom": 3,
+            "maxZoom": 7,
+            "path": "jour-sud",
+            "base": "",
+            "bgcolor": "#333333",
+            "world": "La Falaise - overworld",
+            "last_rendertime": 1652533413,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                188,
+                64,
+                426
+            ],
+            "minZoom": 3,
+            "spawn": false,
+            "north_direction": 2
+        },
+        {
+            "name": "Jour_Est",
+            "zoomLevels": 6,
+            "defaultZoom": 3,
+            "maxZoom": 6,
+            "path": "jour-est",
+            "base": "",
+            "bgcolor": "#333333",
+            "world": "La Falaise - overworld",
+            "last_rendertime": 1652533413,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                188,
+                64,
+                426
+            ],
+            "minZoom": 3,
+            "spawn": false,
+            "north_direction": 3
+        }
+    ]
+};
